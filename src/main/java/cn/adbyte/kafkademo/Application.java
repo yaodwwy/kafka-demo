ba4a980df006617c5d3ac8a6e5aca27f986b2137
Adam Yao <yaodwwy@gmail.com>
2019-10-25 13:49:04 +0800

    Initial commit

---
+++ src/main/java/cn/adbyte/kafkademo/Application.java
@@ -0,0 +1,51 @@
+package cn.adbyte.kafkademo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Adam
+ */
+@Slf4j
+@EnableKafka
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args).close();
+    }
+
+    @Autowired
+    private KafkaTemplate<String, String> template;
+
+    private final CountDownLatch latch = new CountDownLatch(3);
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        for (int i = 0; i < 30; i++) {
+
+            this.template.send("myTopic", i + "我来了");
+            Thread.sleep(200);
+        }
+        latch.await(60, TimeUnit.SECONDS);
+        log.info("All received");
+    }
+
+    @KafkaListener(topics = "myTopic")
+    public void listen(ConsumerRecord<?, ?> cr) throws Exception {
+        log.info(cr.toString());
+        latch.countDown();
+    }
+}
